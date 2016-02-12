24f4c41d29dc3ed333140b07622048960089c64f
vicrtor villacis <victor.villacis@outlook.com>
2016-02-11 20:06:42 -0500

    connected express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var require("express");
+var express = require("express");
+var app = express();  //as needed do things, let the errors drive where you head. Do not skip ahead. 
+var PORT = 3000; // it is smart to have your port up gere
+app.get("/", function (req, res) {
+  res.send("Welcome to the homepage"); //sends to homepage
+
+})
+
+app.listen(PORT, function() {
+  console.log("running on port %s", PORT);
+});
+
